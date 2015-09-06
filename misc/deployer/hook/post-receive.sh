5b0aa089273d377f6d53a6c748cf61de61c6ba2a
Sylvain Artois <sylvaina@alittlemarket.fr>
2015-09-06 10:54:30 +0200

    Create and store a post-receive hook for production bare repository

---
+++ misc/deployer/hook/post-receive.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#From http://www.sitepoint.com/one-click-app-deployment-server-side-git-hooks/
+
+## store the arguments given to the script
+read oldrev newrev refname
+
+## Where to store the log information about the updates
+LOGFILE=./post-receive.log
+
+# The deployed directory (the running site)
+DEPLOYDIR=/srv/contrepensees.fr
+
+##  Record the fact that the push has been received
+echo -e "Received Push Request at $( date +%F )" >> $LOGFILE
+echo " - Old SHA: $oldrev New SHA: $newrev Branch Name: $refname" >> $LOGFILE
+
+## Update the deployed copy
+echo "Starting Deploy" >> $LOGFILE
+
+echo " - Starting code update"
+GIT_WORK_TREE="$DEPLOYDIR" git checkout -f
+echo " - Finished code update"
+
+##Update node modules
+echo " - Starting npm install"
+cd "$DEPLOYDIR"
+npm install;
+echo " - Finished composer update"
+
+##Update bower deps
+echo " - Starting bower install"
+cd "$DEPLOYDIR"
+bower install;
+echo " - Finished composer update"
+
+cd -
+echo "Finished Deploy" >> $LOGFILE
