294269067668fb4d4d2b504d8bfc52186081c8de
Sylvain Artois <sylvaina@alittlemarket.fr>
2015-09-13 11:59:19 +0200

    Add Okab theme in public dir

---
+++ public/config.rb
@@ -0,0 +1,15 @@
+
+# Set this to the root of your project when deployed:
+http_path= "/"
+css_dir= 'css'
+sass_dir= 'styles'
+javascripts_dir= 'js'
+images_dir= 'images'
+fonts_dir= 'fonts'
+relative_assets= true
+boring= true
+debug_info= false
+output_style= :nested
+line_comments = false
+raw= 'preferred_syntax = :scss\n'
+
