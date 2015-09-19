2bcf1b07c95a3def618629617ff159e4a8875d9d
Sylvain Artois <sylvaina@alittlemarket.fr>
2015-09-19 20:19:26 +0200

    Change Gruntfile indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,53 @@
 'use strict()';
 
 var config= {
-	port: 3000
+    port: 3000
 };
 
 module.exports = function(grunt) {
 
-	// Load grunt tasks automatically
-	require('load-grunt-tasks')(grunt);
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
 
-	// Time how long tasks take. Can help when optimizing build times
-	require('time-grunt')(grunt);
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
 
-	var options = {
-		config: {
-			src: './grunt/*.js'
-		},
-		pkg: grunt.file.readJSON('package.json')
-	};
+    var options = {
+        config: {
+            src: './grunt/*.js'
+        },
+        pkg: grunt.file.readJSON('package.json')
+    };
 
-	var configs = require('load-grunt-configs')(grunt, options);
-	
-	// Project configuration.
-	grunt.initConfig(configs);
+    var configs = require('load-grunt-configs')(grunt, options);
 
-	// load jshint
-	grunt.registerTask('lint', [
-		'jshint'
-	]);
+    // Project configuration.
+    grunt.initConfig(configs);
 
-	grunt.registerTask('dev', [
-		'sass',
-		'watch'
-	]);
+    // load jshint
+    grunt.registerTask('lint', [
+        'jshint'
+    ]);
+
+    grunt.registerTask('dev', [
+        'sass',
+        'watch'
+    ]);
 
     grunt.registerTask('front', [
         'compass',
         'postcss'
     ]);
 
-	// default option to connect server
-	grunt.registerTask('serve', [
-		'jshint',
-		'concurrent:dev'
-	]);
+    // default option to connect server
+    grunt.registerTask('serve', [
+        'jshint',
+        'concurrent:dev'
+    ]);
 
-	grunt.registerTask('server', function () {
-		grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-		grunt.task.run(['serve:' + target]);
-	});
+    grunt.registerTask('server', function () {
+        grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+        grunt.task.run(['serve:' + target]);
+    });
 
 };
