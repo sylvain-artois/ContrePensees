5643d811de280173c7b1bde99359f20a8dd2e3b6
Sylvain Artois <sylvaina@alittlemarket.fr>
2015-09-20 15:12:36 +0200

    Update keystone admin nav

--- keystone.js
+++ keystone.js
@@ -13,23 +13,23 @@ var nunjucks = require('nunjucks');
 
 keystone.init({
 
-	'name': 'Contre Pensées',
-	'brand': 'Contre Pensées',
-	
-	'sass': 'public',
-	'static': 'public',
-	'favicon': 'public/favicon.ico',
-	'views': 'templates/views',
-	'view engine': 'html',
-	'custom engine': cons.nunjucks,
-	
-	'emails': 'templates/emails',
-	
-	'auto update': true,
-	'session': true,
-	'auth': true,
-	'user model': 'User',
-	'cookie secret': '&C=:4,`XEOaLc9m]F{?q!$PR0F/5R[5%C%AaQe<TQ[[4E&d5E?Eo?%HN?DGiWOSa'
+    'name': 'Contre Pensées',
+    'brand': 'Contre Pensées',
+
+    'sass': 'public',
+    'static': 'public',
+    'favicon': 'public/favicon.ico',
+    'views': 'templates/views',
+    'view engine': 'html',
+    'custom engine': cons.nunjucks,
+
+    'emails': 'templates/emails',
+
+    'auto update': true,
+    'session': true,
+    'auth': true,
+    'user model': 'User',
+    'cookie secret': '&C=:4,`XEOaLc9m]F{?q!$PR0F/5R[5%C%AaQe<TQ[[4E&d5E?Eo?%HN?DGiWOSa'
 
 });
 
@@ -42,10 +42,10 @@ keystone.import('models');
 // for each request) should be added to ./routes/middleware.js
 
 keystone.set('locals', {
-	_: require('underscore'),
-	env: keystone.get('env'),
-	utils: keystone.utils,
-	editable: keystone.content.editable
+    _: require('underscore'),
+    env: keystone.get('env'),
+    utils: keystone.utils,
+    editable: keystone.content.editable
 });
 
 // Load your project's Routes
@@ -56,18 +56,18 @@ keystone.set('routes', require('./routes'));
 // default email templates, you may remove them if you're using your own.
 
 keystone.set('email locals', {
-	logo_src: '/images/logo-email.gif',
-	logo_width: 194,
-	logo_height: 76,
-	theme: {
-		email_bg: '#f9f9f9',
-		link_color: '#2697de',
-		buttons: {
-			color: '#fff',
-			background_color: '#2697de',
-			border_color: '#1a7cb7'
-		}
-	}
+    logo_src: '/images/logo-email.gif',
+    logo_width: 194,
+    logo_height: 76,
+    theme: {
+        email_bg: '#f9f9f9',
+        link_color: '#2697de',
+        buttons: {
+            color: '#fff',
+            background_color: '#2697de',
+            border_color: '#1a7cb7'
+        }
+    }
 });
 
 // Setup replacement rules for emails, to automate the handling of differences
@@ -77,11 +77,11 @@ keystone.set('email locals', {
 // other rules your email templates require.
 
 keystone.set('email rules', [{
-	find: '/images/',
-	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/images/' : 'http://localhost:3000/images/'
+    find: '/images/',
+    replace: (keystone.get('env') == 'production') ? 'http://contrepensees.fr/images/' : 'http://localhost:3000/images/'
 }, {
-	find: '/keystone/',
-	replace: (keystone.get('env') == 'production') ? 'http://www.your-server.com/keystone/' : 'http://localhost:3000/keystone/'
+    find: '/keystone/',
+    replace: (keystone.get('env') == 'production') ? 'http://contrepensees.fr/keystone/' : 'http://localhost:3000/keystone/'
 }]);
 
 // Load your project's email test routes
@@ -91,10 +91,10 @@ keystone.set('email tests', require('./routes/emails'));
 // Configure the navigation bar in Keystone's Admin UI
 
 keystone.set('nav', {
-	'posts': ['posts', 'post-categories'],
-	'galleries': 'galleries',
-	'enquiries': 'enquiries',
-	'users': 'users'
+    'user': 'users',
+    'category': 'categories',
+    'enquiry': 'enquiries',
+    'content': ['galleries', 'media', 'photos', 'posts', 'quotes']
 });
 
 // Start Keystone to connect to your database and initialise the web server
