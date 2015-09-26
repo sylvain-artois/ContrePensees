ce08b7691830bfbdd97da41b7f997a7a2378b2c5
Sylvain Artois <sylvaina@alittlemarket.fr>
2015-09-26 12:36:05 +0200

    Minor view improvment

--- routes/views/blog.js
+++ routes/views/blog.js
@@ -30,7 +30,7 @@ exports = module.exports = function(req, res) {
             // Load the counts for each category
             async.each(locals.data.categories, function(category, next) {
 
-                keystone.list('Post').model.count().where('category').in([category.id]).exec(function(err, count) {
+                keystone.list('Post').model.count().where('categories').in([category.id]).exec(function(err, count) {
                     category.postCount = count;
                     next(err);
                 });
@@ -57,114 +57,22 @@ exports = module.exports = function(req, res) {
     // Load the posts
     view.on('init', function(next) {
 
-        var q1 = keystone.list('Post').paginate({
+        var q = keystone.list('Post').paginate({
                 page: req.query.page || 1,
                 perPage: 10,
                 maxPages: 10
             })
             .where('state', 'published')
             .sort('-publishedDate')
-            .populate('author category');
+            .populate('author categories');
 
         if (locals.data.category) {
-            q1.where('category').in([locals.data.category]);
+            q.where('categories').in([locals.data.category]);
         }
 
-        var q2 = keystone.list('Quote').paginate({
-                page: req.query.page || 1,
-                perPage: 10,
-                maxPages: 10
-            })
-            .where('state', 'published')
-            .sort('-publishedDate')
-            .populate('author category');
-
-        if (locals.data.category) {
-            q2.where('category').in([locals.data.category]);
-        }
-
-        var q3 = keystone.list('Photo').paginate({
-                page: req.query.page || 1,
-                perPage: 10,
-                maxPages: 10
-            })
-            .where('state', 'published')
-            .sort('-publishedDate')
-            .populate('author category');
-
-        if (locals.data.category) {
-            q3.where('category').in([locals.data.category]);
-        }
-
-        var q4 = keystone.list('Photo').paginate({
-                page: req.query.page || 1,
-                perPage: 10,
-                maxPages: 10
-            })
-            .where('state', 'published')
-            .sort('-publishedDate')
-            .populate('author category');
-
-        if (locals.data.category) {
-            q4.where('category').in([locals.data.category]);
-        }
-
-        async.parallel([
-            function(callback){
-                q1.exec(function(err, results) {
-                    if (err) {
-                        callback(err);
-                    } else {
-                        callback(null, results);
-                    }
-                });
-            },
-            function(callback){
-                q2.exec(function(err, results) {
-                    if (err) {
-                        callback(err);
-                    } else {
-                        callback(null, results);
-                    }
-                });
-            },
-            function(callback){
-                q3.exec(function(err, results) {
-                    if (err) {
-                        callback(err);
-                    } else {
-                        callback(null, results);
-                    }
-                });
-            },
-            function(callback){
-                q4.exec(function(err, results) {
-                    if (err) {
-                        callback(err);
-                    } else {
-                        callback(null, results);
-                    }
-                });
-            }
-        ], function(err, results){
-            if (err) {
-                next(err);
-            } else {
-                console.log( results[0].results.length + results[1].results.length + results[2].results.length + results[3].results.length);
-                var mixedPost = results[0].results.concat(results[1].results, results[2].results, results[3].results);
-                console.log(mixedPost.length);
-                mixedPost = mixedPost.sort(function(a, b) {
-                    var x = a['publishedDate'],
-                        y = b['publishedDate'];
-                    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-                });
-                console.log(mixedPost.length);
-                results.results = mixedPost.slice(0, 10);
-                console.log(results.results.length);
-
-                locals.data.posts = results;
-                next(null);
-            }
+        q.exec(function(err, results) {
+            locals.data.posts = results;
+            next(err);
         });
     });
 
