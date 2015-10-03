845665187b565fcc12ec51d110a212444063d7f1
Sylvain Artois <sylvaina@alittlemarket.fr>
2015-10-03 09:54:57 +0200

    [master] Introduce sitemap plugin

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,7 @@
-/**
- * This file is where you define your application routes and controllers.
- * 
- * Start by including the middleware you want to run for every request;
- * you can attach middleware to the pre('routes') and pre('render') events.
- * 
- * For simplicity, the default setup for route controllers is for each to be
- * in its own file, and we import all the files in the /routes/views directory.
- * 
- * Each of these files is a route controller, and is responsible for all the
- * processing that needs to happen for the route (e.g. loading data, handling
- * form submissions, rendering the view template, etc).
- * 
- * Bind each route pattern your application should respond to in the function
- * that is exported from this module, following the examples below.
- * 
- * See the Express application routing documentation for more information:
- * http://expressjs.com/api.html#app.VERB
- */
+var keystone   = require('keystone'),
+    middleware = require('./middleware'),
+    sitemap    = require('keystone-express-sitemap');
 
-var keystone = require('keystone');
-var middleware = require('./middleware');
 var importRoutes = keystone.importer(__dirname);
 
 // Common Middleware
@@ -34,7 +16,6 @@ var routes = {
 // Setup Route Bindings
 exports = module.exports = function(app) {
 
-    // Views
     app.get('/', routes.views.index);
     app.get('/blog/:category?', routes.views.blog);
     app.get('/blog/post/:post', routes.views.post);
@@ -42,4 +23,8 @@ exports = module.exports = function(app) {
     app.get('/dye-pop/portfolio', routes.views.portfolio);
     app.all('/contact', routes.views.contact);
     app.all('/search/:query?',  routes.views.search);
+
+    app.get('/sitemap.xml', function(req, res) {
+        sitemap.create(keystone, req, res);
+    });
 };
