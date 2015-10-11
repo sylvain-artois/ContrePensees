0cc631ac79570edf4e7d4ad830f7063f6eb32a0c
Sylvain Artois <sylvaina@alittlemarket.fr>
2015-10-11 16:40:36 +0200

    [MASTER] Update sass project

--- assets/styles/config.rb
+++ assets/styles/config.rb
@@ -1,16 +1,7 @@
-
-# Set this to the root of your project when deployed:
 http_path= "/"
 project_path="public"
+sass_path="assets/styles/sass"
 css_dir="css"
-sass_dir="styles/front"
-javascripts_dir="js"
 images_dir="images"
 fonts_dir="fonts"
 relative_assets= true
-boring= true
-debug_info= false
-output_style= :nested
-line_comments = false
-raw= 'preferred_syntax = :scss\n'
-
