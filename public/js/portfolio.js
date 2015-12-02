23861657f670d04b597beb6d2d1c1ff8e3fb8cf6
Sylvain Artois <sylvaina@alittlemarket.fr>
2015-12-02 09:17:06 +0100

    [portfolio-with-es6] Create js file for portfolio, and update html code

---
+++ public/js/portfolio.js
@@ -0,0 +1,8 @@
+import {OffCanvasMenu} from 'lib/OffCanvasMenu.js'
+import {Slider} from 'lib/Slider.js'
+
+new OffCanvasMenu({
+        name: 'sidr-main',
+        source: '.dima-navbar nav'
+    })
+    .init('.dima-btn-nav');
