c46f83b7317a06978a4e101fb2cce17504bccbe4
Sylvain Artois <sylvaina@alittlemarket.fr>
2015-12-20 19:45:59 +0100

    [new-home] Fetch posts by category

--- routes/views/home.js
+++ routes/views/home.js
@@ -1,12 +1,44 @@
-var keystone = require('keystone');
+var keystone = require('keystone'),
+    async = require('async');
 
 exports = module.exports = function(req, res) {
 
-    res.locals.section = 'home';
-    res.locals.data = {
-        categories: res.locals.categories,
-        env: keystone.get('env')
+    var view = new keystone.View(req, res);
+    var locals = res.locals;
+
+    locals.section = 'home';
+    locals.data = {
+        categories: locals.categories,
+        env: keystone.get('env'),
+        postByCategory: {}
     };
 
-    new keystone.View(req, res).render('home');
+    // Load last three updated post for each category
+    view.on('init', function(next) {
+        async.each(locals.data.categories, function(category, callback) {
+
+            keystone.list('Post').model.find()
+                .where('state', 'published')
+                .where('categories').in([category])
+                .populate('author categories')
+                .sort('-publishedDate')
+                .limit(3)
+                .exec(function(err, documents) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    category.posts = documents;
+                    locals.data.postByCategory[category.key] = category
+                    callback();
+                });
+        }, function(err) {
+            if  (err) {
+                return next(err);
+            }
+            next();
+        });
+    });
+
+    view.render('home');
 };
