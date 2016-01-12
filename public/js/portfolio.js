33d8b4bbf10601fad416771ae9f505a67fff79ab
Sylvain Artois <sylvaina@alittlemarket.fr>
2016-01-12 22:51:24 +0100

    [master] Fix missing Off canvas menu in portfolio

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,11 +1,11 @@
-//import {OffCanvasMenu}  from 'lib/OffCanvasMenu.js';
+import {OffCanvasMenu}  from 'lib/OffCanvasMenu.js';
 import {GalleryHandler} from 'lib/GalleryHandler.js';
 
-/*new OffCanvasMenu({
+new OffCanvasMenu({
         name: 'sidr-main',
         source: '.dima-navbar nav'
     })
-    .init('.dima-btn-nav');*/
+    .init('.dima-btn-nav');
 
 new GalleryHandler({
         selector: '.gallery',
---
+++ public/js/resume.js
@@ -0,0 +1,3 @@
+/**
+ * Created by sartois on 12/01/2016.
+ */
