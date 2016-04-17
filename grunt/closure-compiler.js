166170acdc7fae216b93505aad5a715ff0132aa2
Sylvain Artois <sylvain.artois@gmail.com>
2016-04-17 10:35:08 +0200

    [fix-minor-bug-js-transpilation] Update grunt port. Fix sidr missing min file

--- grunt/closure-compiler.js
+++ grunt/closure-compiler.js
@@ -1,3 +1,7 @@
+/**
+ * java -jar node_modules/google-closure-compiler/compiler.jar 
+ * @type {{home: {files: {[public/js/bundle/home.min.js]: string[]}, options: {compilation_level: string, language_in: string, language_out: string, create_source_map: string, externs: string[]}}}}
+ */
 module.exports = {
     home: {
         files: {
@@ -14,7 +18,7 @@ module.exports = {
             create_source_map: 'public/js/bundle/home.min.js.map',
             externs: [
                 'public/js/vendor/jquery.min.js',
-                'public/js/vendor/jquery.sidr.min.js'
+                'public/js/vendor/jquery.sidr.js'
             ]
         }
     }
